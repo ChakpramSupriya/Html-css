a0b524daae7ce4cbd9d70a4bc5f4c5dba6704a1e
ChakpramSupriya <chakpramsupriya@gmail.com>
2024-03-09 11:42:17 +0530

    day3

---
+++ dom_day3/script.js
@@ -0,0 +1,13 @@
+// console.log(document.getElementsByTagName("li"));
+
+
+// let d = document.getElementById("child").children[1]
+// d.innerText = "Abem";
+// console.log(d);
+
+// let n = document.getElementById("child").children[1].parentNode.parentNode;
+// console.log(n);
+
+
+let n = document.getElementById("child").children[2].previousElementSibling.previousElementSibling;
+console.log(n);
