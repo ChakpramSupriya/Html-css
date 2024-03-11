8f26b8759148d9dd753c84a1fcee060a4230f54d
ChakpramSupriya <chakpramsupriya@gmail.com>
2024-03-11 11:06:27 +0530

    day 5

---
+++ netflix_clone/script.js
@@ -0,0 +1,190 @@
+let movies = [
+    {
+        name:"Falling into your smile",
+        poster:"https://e1.pxfuel.com/desktop-wallpaper/518/661/desktop-wallpaper-falling-into-your-smile-thumbnail.jpg",
+        cast:"Xu kai , Cheng Xiao",
+        rating:8.6
+    },
+    {
+        name:"Love Scenary",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpy27Sx8ujBLPpHIckqR1Uw8t04nAGwfHUUA&usqp=CAU",
+        cast:"Lin Yi , Xu Yu",
+        rating:8.4
+    },
+    {
+        name:"My Lethal man",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS15g6v-X3HsDSKOLRamvcn2l9o3lztG8i9GQ&usqp=CAU",
+        cast:"Fna Xhizin , Mozhi Li",
+        rating:8.1
+    },
+    {
+        name:"Wonderland of Love",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEEqiOfxGqD52iRU7QL8Wd835cW2rXjcMexQ&usqp=CAU",
+        cast:"Xu kai , Jing Tian",
+        rating:8.5
+    },
+    {
+        name:"Love 020",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKgDa4K1TnDhGPCC2DodmzZH0XUfXqGFNQXUxXbGy3URByoJaWXLAuaZsyYxE4LIVEL0M&usqp=CAU",
+        cast:"Yang Yang , Zheng Shuang",
+        rating:8.3
+    },
+    {
+        name:"Mr Bad",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS88hcXQwiSh3FwxxtGiwr132N04pMX9LYhmQM5_lWj45h2r6cNItSFOFFi7qVVcTeAYU8&usqp=CAU",
+        cast:"Chen Zheyuan , Shen Yue",
+        rating:8.1
+    },
+    {
+        name:"Only for Love",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7AyF0VIBJ538CZ6kHaR2TZ9pW_ovK1ATCE0gXkpnXbdgMr4AyQ6cazzocwf1vjIL5RKk&usqp=CAU",
+        cast:"Dylan Wang , Bai Lu",
+        rating:7.9
+    },
+    {
+        name:"Got a crush on you",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3pVDlxrpOt0o2H-Lql4IJ0EYS6Rv8i60hvDyLv2B2uN47VvyvPMpeX7f_4zZm-piXFYI&usqp=CAU",
+        cast:"Xu Kai Cheng , Gulnezer Bextiyar",
+        rating:7.5
+    },
+    {
+        name:"Love my voice",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8k8uRe2oAFS2uO7TxfkPYu8b5WEhZV51oRA&usqp=CAU",
+        cast:"Tan Jian Ci , Zhou Ye",
+        rating:8.5
+    },
+    {
+        name:"Rattan",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIvWvwVbFeOpjy0IppfJ8JIIbg1W0lVgCQEQ&usqp=CAU",
+        cast:"Vin Zhang , Jian Tian",
+        rating:8
+    },
+    {
+        name:"Mr Swimmer",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmjoqxV_mE4vBOhg6d-KbkE4syk53G9APwDEwJYnEJw4i9hAonA3OLznN99uyLebERRCQ&usqp=CAU",
+        cast:"Mike Angelo , Ju Jingyi",
+        rating:"7.4"
+    },
+    {
+        name:"Love like galaxy",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNkBd_4I2tFGsiBNfm3xWJju7nEKSL0hZ7OQ&usqp=CAU",
+        cast:"Leo Wu , Zhao Lusi",
+        rating:"9"
+    },
+    {
+        name:"My Demon",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh74DduRopfPsCEprghS5J0hZ9qNPDXaWEiHdJT9Wa3IDlEvpKk-u8gIYpB2NrB9Nu8s8&usqp=CAU",
+        cast:"Song Kang , Kim Yoo-Jung",
+        rating:"8.5"
+    },
+    {
+        name:"My Love from the star",
+        poster:"https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/You_Who_Came_From_the_Stars_Cover.jpg/220px-You_Who_Came_From_the_Stars_Cover.jpg",
+        cast:"Kim Soo-Hyun , Jun Ji-Hyun",
+        rating:"8.5"
+    },
+    {
+        name:"A love so beautiful",
+        poster:"https://e1.pxfuel.com/desktop-wallpaper/909/832/desktop-wallpaper-video-first-posters-and-teaser-added-for-the-upcoming-korean-drama-a-love-so-beautiful-hancinema.jpg",
+        cast:"Hu Yitian , Shen Yue",
+        rating:"8.3"
+
+    },
+    {
+        name:"Mr Romantic 3",
+        poster:"https://th-i.thgim.com/public/entertainment/movies/eg0e3e/article66842104.ece/alternates/FREE_1200/tale%20of%20nine%20tailed.jpg",
+        cast:"An Hyo Seop , Lee Sung Kyung",
+        rating:"9.0"
+    },
+    {
+        name:"Alice",
+        poster:"https://i.pinimg.com/originals/8d/78/a5/8d78a5af532bc963a93239d6df8e306c.jpg",
+        cast: "Joo Won , Kim Hee-Sun",
+        rating:"7.4"
+    },
+    {
+        name:"Run On",
+        poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO0MrSyx050hpZenQ_Jx7M-uKiQ908ErwyMyUuSnhhY6ccjwfY_alP0AgONhwoMIhLmbw&usqp=CAU",
+        cast: "Im Si-Wan , Shin Se-Kyung",
+        rating:"8.2"
+    }
+];
+
+
+
+
+function searchMovie()
+{
+    let movieName = document.getElementById('search').value;
+    if(movieName!=="")
+    {
+        let result = movies.filter(function(movie)
+        {
+            return movie.name.toUpperCase().includes(movieName.toUpperCase())
+        })
+        // console.log(result);
+
+        // if(result.length==0)
+        // {
+        //     
+        // }
+        displayMovies(result);
+    }
+    else
+    {
+        displayMovies(movies);
+    }
+
+    // console.log(movieName);
+}
+
+// parameter pass
+function displayMovies(data)
+{
+
+    document.getElementById("movies").innerHTML="";
+
+    let htmlString = ``;
+
+    for(let i=0;i<data.length;i++)
+    {
+        htmlString=htmlString+`
+        <div class="movie">
+            <div class="overlay">
+                <div class="video">
+
+                </div>  
+
+                <div class="details">
+                    <h1>${data[i].name}</h1>
+                    <h2>IMDB:${data[i].rating}</h2>
+                    <P> Cast : ${data[i].cast}</P>
+                </div>
+            </div>
+            <img class="poster" src="${data[i].poster}" alt="poster">
+        </div>
+        `
+        
+    }
+
+    console.log(htmlString);
+    document.getElementById("movies").innerHTML=htmlString;
+
+    // // <div class="movie"></div>
+    // let movieDIV = document.createElement("div");
+    // movieDIV.classList.add("movie");
+
+    // let overlayDIV = document.createElement("div");
+    // overlayDIV.classList.add("overlay");
+
+    // movieDIV.appendChild(overlayDIV);
+
+    // let videoDIV = document.createElement("div");
+    // videoDIV.classList.add("video");
+
+    // overlayDIV.appendChild(videoDIV);
+
+    
+    // console.log(movieDIV);
+}
+displayMovies(movies);
